92f1b8d83916ed700b5ff17645624db7d11f861d
= <andylackk@outlook.com>
2025-03-16 16:33:37 -0500

    log in functionallity works with Context API

--- reimbursement-frontend/src/Components/Login.tsx
+++ reimbursement-frontend/src/Components/Login.tsx
@@ -3,79 +3,96 @@ import TextField from "@mui/material/TextField";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { employeeAuth } from "../GlobalData/AuthContext";
 import { LoginProps } from "../InterFaces/LoginProps";
-import { store } from "../GlobalData/store";
 
+export const Login: React.FC<LoginProps> = ({ setCurrentEmployeeid }) => {
+  //!NEW! Context API
+  const { loggedInEmployee, setLoggedInEmployee } = employeeAuth();
 
-export const Login:React.FC<LoginProps> = ({setCurrentEmployeeid}) => {
+  const navigate = useNavigate();
 
+  const userNameRef = useRef<HTMLInputElement>(null);
 
-    const navigate = useNavigate()
-
-
-    const userNameRef = useRef<HTMLInputElement>(null);
-
-    useEffect(()=>{
-        if(userNameRef.current){
-            userNameRef.current.focus();
-        }
-    })
-
-    const[loginCreds, setLoginCreds] = useState({
-        username: "",
-        password: ""
-    })
-
-    const storeValues = (event:React.ChangeEvent<HTMLInputElement>) => {
-        const name = event.target.name;
-        const value = event.target.value;
-
-        setLoginCreds((loginCreds)=>({...loginCreds,[name]: value}))
+  useEffect(() => {
+    if (userNameRef.current) {
+      userNameRef.current.focus();
     }
+  }, []);
 
-    const login = async () => {
-        try{
-            // const response = await axios.post("http://localhost:8080/auth/login",loginCreds,{withCredentials:true})
+  const [loginCreds, setLoginCreds] = useState({
+    username: "",
+    password: "",
+  });
 
+  const [loggedIn, setLoggedIn] = useState(false);
 
-            const response = await axios.post("http://localhost:8080/auth/login",loginCreds,{withCredentials:true})
-            store.loggedInEmployee = response.data;
-            const currentEmployee = store.loggedInEmployee;
-            setCurrentEmployeeid(currentEmployee.employeeid);
-            alert(store.loggedInEmployee.username + " has logged in , welcome.  You are a " + store.loggedInEmployee.title + " employee");
+  const storeValues = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
 
-            // setLoggedInEmployee(response.data)
-            // setCurrentEmployeeid(response.data.employeeid)
+    setLoginCreds((loginCreds) => ({ ...loginCreds, [name]: value }));
+  };
 
-            // alert("Welcome " + loggedInEmployee?.username)
+  const login = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/auth/login",
+        loginCreds,
+        { withCredentials: true }
+      );
 
-
-            if(store.loggedInEmployee?.title == "manager"){
-                navigate("/reimb")
-            } else {
-                navigate("/basic")
-            }
-        } catch{
-            alert("login unsuccessful")
-        }
+      //!NEW! Context API
+      setLoggedInEmployee(response.data);
+      setLoggedIn(true);
+    } catch {
+      alert("login unsuccessful");
     }
+  };
 
+  useEffect(() => {
+    if (loggedIn && loggedInEmployee) {
+      alert(
+        `${loggedInEmployee.username} has logged in, welcome. You are a ${loggedInEmployee.title} employee`
+      );
 
-    return(
+      if (loggedInEmployee.title === "manager") {
+        navigate("/reimb");
+      } else {
+        navigate("/basic");
+      }
+    }
+  }, [loggedIn, loggedInEmployee, navigate]);
 
-        <div className="centered">
-            <h1>Log In</h1>
+  return (
+    <div className="centered">
+      <h1>Log In</h1>
 
-            <TextField ref={userNameRef} name="username" id="outlined-basic" label="User Name" variant="outlined" onChange={storeValues}/>
-            <TextField type="password" name="password" id="outlined-basic" label="Password" variant="outlined" onChange={storeValues}/>
+      <TextField
+        ref={userNameRef}
+        name="username"
+        id="outlined-basic"
+        label="User Name"
+        variant="outlined"
+        onChange={storeValues}
+      />
+      <TextField
+        type="password"
+        name="password"
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        onChange={storeValues}
+      />
 
-
-            <div>
-            <Button  onClick={login} variant="contained">Log In</Button>
-            <Button onClick={()=>navigate("/register")} variant="contained">Register</Button>
-            </div>
-
-        </div>
-
-    )
-}
+      <div>
+        <Button onClick={login} variant="contained">
+          Log In
+        </Button>
+        <Button onClick={() => navigate("/register")} variant="contained">
+          Register
+        </Button>
+      </div>
+    </div>
+  );
+};
